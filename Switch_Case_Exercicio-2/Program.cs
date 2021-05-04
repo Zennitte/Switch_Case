19e5f019d55d869e43fe00d23ec6f17a481356f3
Kauê Camargo <camargokaue92@gmail.com>
2021-05-04 14:12:44 -0300

    Exercicios_Switch-Case

---
+++ Switch_Case_Exercicio-2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Switch_Case_Exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um algarismo romano de 1 a 10");
+            string numeroRomano = Console.ReadLine().ToUpper();
+
+            switch (numeroRomano)
+            {
+                case "I":
+                Console.WriteLine("Seu número romano em arábico é igual 1");
+                    break;
+                case "II":
+                Console.WriteLine("Seu número romano em arábico é igual 2");
+                    break;
+                case "III":
+                Console.WriteLine("Seu número romano em arábico é igual 3");
+                    break;
+                case "IV":
+                Console.WriteLine("Seu número romano em arábico é igual 4");
+                    break;
+                case "V":
+                Console.WriteLine("Seu número romano em arábico é igual 5");
+                    break;
+                case "VI":
+                Console.WriteLine("Seu número romano em arábico é igual 6");
+                    break;
+                case "VII":
+                Console.WriteLine("Seu número romano em arábico é igual 7");
+                    break;
+                case "VIII":
+                Console.WriteLine("Seu número romano em arábico é igual 8");
+                    break;
+                case "IX":
+                Console.WriteLine("Seu número romano em arábico é igual 9");
+                    break;
+                case "X":
+                Console.WriteLine("Seu número romano em arábico é igual 10");
+                    break;
+                default:
+                Console.WriteLine("Reinicie o algoritimo e digite um número válido");
+                    break;
+            }
+            
+        }
+    }
+}
---
+++ Switch_Case_Exercício-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome");
+            string nome = Console.ReadLine().ToLower();                       
+            string letra = nome.Substring(0, 1);
+
+            switch (letra)
+            {
+                case "a":
+                Console.WriteLine("Seu nome começa com a letra " + letra + " de amor");
+                    break;
+                case "e":
+                Console.WriteLine("Seu nome começa com a letra " + letra + " de escola");
+                    break;
+                case "i":
+                Console.WriteLine("Seu nome começa com a letra " + letra + " de igreja");
+                    break;
+                case "o":
+                Console.WriteLine("Seu nome começa com a letra " + letra + " de ovo");
+                    break;
+                case "u":
+                Console.WriteLine("Seu nome começa com a letra " + letra + " de uva");
+                    break;
+                default:
+                Console.WriteLine("Seu nome não começa com uma vogal");
+                    break;
+            }
+            
+        }
+    }
+}
